6db7272bf464df464ffe8b253195f7121ed94920
Kaivan Kamali <kxk302@psu.edu>
2023-09-20 17:05:19 -0400

    Concat script now can concat bed files. Length script can now calculate read sizes for fasta files with multiple reads

--- scripts/concat_non_b_dna_files.py
+++ scripts/concat_non_b_dna_files.py
@@ -5,19 +5,22 @@ from os import path
 import pandas as pd
 
 
-def concat_nBMST_output_files(input_dir, output_file, chromosome_name, non_b_dna_type):
+def concat_nBMST_output_files(input_dir, output_file, chromosome_name, non_b_dna_type, input_file_extension):
   if non_b_dna_type is None:
-    file_names = glob.glob(path.join(input_dir, chromosome_name + "*.tsv"))
+    file_names = glob.glob(path.join(input_dir, chromosome_name + "_*" + input_file_extension))
   else:
-    file_names = glob.glob(path.join(input_dir, chromosome_name + "_" + non_b_dna_type + ".tsv"))
+    file_names = glob.glob(path.join(input_dir, chromosome_name + "_" + non_b_dna_type + input_file_extension))
 
   df_all = pd.DataFrame()
 
   for file_name in file_names:
     print(f"file_name: {file_name}")
-    df = pd.read_csv(file_name, sep='\t')[["Start", "Stop", "Strand"]]
-    df.rename(columns={"Start": "chromStart", "Stop": "chromEnd", "Strand": "strand"}, inplace=True)
-    df.insert(0, "chrom", chromosome_name)
+    if input_file_extension == ".tsv":
+      df = pd.read_csv(file_name, sep='\t')[["Start", "Stop", "Strand"]]
+      df.rename(columns={"Start": "chromStart", "Stop": "chromEnd", "Strand": "strand"}, inplace=True)
+      df.insert(0, "chrom", chromosome_name)
+    elif input_file_extension == ".bed":
+      df = pd.read_csv(file_name, sep='\t', names=["chrom", "chromStart", "chromEnd", "strand"])
     df.chromStart = df.chromStart.astype(int)
     df.chromEnd = df.chromEnd.astype(int)
     print(df.head(1))
@@ -33,7 +36,8 @@ if __name__ == "__main__":
   argParser = argparse.ArgumentParser("Concatenate nBMST output files")
   argParser.add_argument("-i", "--input_dir", type=str, required=True)
   argParser.add_argument("-o", "--output_file", type=str, required=True)
-  argParser.add_argument("-c", "--chromosome_name", type=str, required=True, choices=["chrX", "chrY"])
+  argParser.add_argument("-c", "--chromosome_name", type=str, required=True)
   argParser.add_argument("-n", "--non_b_dna_type", type=str, choices=["GQ", "MR", "STR", "APR", "IR", "DR", "Z"])
+  argParser.add_argument("-e", "--input_file_extension", type=str, default=".tsv")
   args = argParser.parse_args()
-  concat_nBMST_output_files(args.input_dir, args.output_file, args.chromosome_name, args.non_b_dna_type)
+  concat_nBMST_output_files(args.input_dir, args.output_file, args.chromosome_name, args.non_b_dna_type, args.input_file_extension)
--- scripts/get_chr_length.py
+++ scripts/get_chr_length.py
@@ -3,20 +3,31 @@ import argparse
 import Bio.SeqIO as IO
 
 
-def get_chr_length(chromosome_fasta_file, chromosome_name, output_file):
-  record_dict = IO.to_dict(IO.parse(chromosome_fasta_file, "fasta"))
-  chromosome_sequence_length = len(record_dict[chromosome_name].seq)
-  print(f'chrX sequence length: {chromosome_sequence_length}')
-  with open(output_file, "w") as fp:
-    fp.write(f'{chromosome_name}\t{chromosome_sequence_length}')
+def get_chr_length(chromosome_fasta_file, chromosome_name, file_extension):
+  record_dict = IO.to_dict(IO.parse(chromosome_fasta_file, file_extension))
+  if chromosome_name is None:
+    chromosome_sequence_length = 0
+    for value in record_dict.values():
+      chromosome_sequence_length += len(value.seq)
+    return chromosome_sequence_length
+  else:
+    chromosome_sequence_length = len(record_dict[chromosome_name].seq)
+    return chromosome_sequence_length
 
 
 if __name__ == "__main__":
   argParser = argparse.ArgumentParser("This script returns the length of a chromosome given a fatsa file")
 
   argParser.add_argument("-i", "--chromosome_fasta_file", type=str, required=True)
-  argParser.add_argument("-c", "--chromosome_name", type=str, required=True)
+  argParser.add_argument("-c", "--chromosome_name", type=str)
   argParser.add_argument("-o", "--output_file", type=str, required=True)
+  argParser.add_argument("-e", "--file_extension", type=str, default="fasta")
   args = argParser.parse_args()
   
-  get_chr_length(args.chromosome_fasta_file, args.chromosome_name, args.output_file)
+  chromosome_sequence_length = get_chr_length(args.chromosome_fasta_file, args.chromosome_name, args.file_extension)
+  print(f'chromosome sequence length: {chromosome_sequence_length}')
+  with open(args.output_file, "w") as fp:
+    if args.chromosome_name is None:
+      fp.write(f'{chromosome_sequence_length}')
+    else:
+      fp.write(f'{args.chromosome_name}\t{chromosome_sequence_length}')
